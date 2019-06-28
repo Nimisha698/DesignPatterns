38a9d64526609277ae56517979e27f204c49c06f
Nimisha <nimisha.sanjay@zemosolabs.com>
2019-06-28 12:35:09 +0530

    adding singleton

--- singleton/Singleton.java
+++ singleton/Singleton.java
@@ -1,4 +1,18 @@
 package singleton;
 
-public class Singleton {
+import java.util.*;
+class Singleton
+{
+    String string;
+    private static Singleton single=null;
+    private Singleton()
+    {
+        string="String of Singleton class";
+    }
+    public static Singleton getInstance()
+    {
+        if(single==null)
+            return single=new Singleton();
+        return single;
+    }
 }
