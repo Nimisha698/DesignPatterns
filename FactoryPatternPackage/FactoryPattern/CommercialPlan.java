d88a0da0b750a9b28135874f6825c4af045a0da3
Nimisha <nimisha.sanjay@zemosolabs.com>
2019-06-28 12:31:28 +0530

    deleting factory

--- FactoryPatternPackage/FactoryPattern/CommercialPlan.java
+++
@@ -1,9 +0,0 @@
-package FactoryPatternPackage.FactoryPattern;
-
-public class CommercialPlan extends Plan
-{
-    public void getRate()
-    {
-        rate=6.50;
-    }
-}
--- FactoryPatternPackage/FactoryPattern/DomesticPlan.java
+++
@@ -1,9 +0,0 @@
-package FactoryPatternPackage.FactoryPattern;
-
-public class DomesticPlan extends Plan
-{
-    public void getRate()
-    {
-        rate=3.50;
-    }
-}
